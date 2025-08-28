dc180b660f1a9991ee30634c35130fe826050fbb
zwyyy456 <zwyyy456@gmail.com>
2025-08-28 14:42:06 +0800

    feat: 支持中国银行信用卡账单解析

--- src/adapters/index.ts
+++ src/adapters/index.ts
@@ -4,26 +4,28 @@ import { BocomDebitAdapter } from './bocom-debit';
 import { CmbCreditAdapter } from './cmb-credit'
 import { CmbDebitAdapter } from './cmb-debit'
 import { BocDebitAdapter } from './boc-debit'
+import { BocCreditAdapter } from './boc-credit'
 import { Adapter } from './types';
 import { compareKey } from '../utils';
 
 export const AdapterList = [
-  AbcDebitAdapter,
-  CmbCreditAdapter,
-  CmbDebitAdapter,
-  BocomCreditAdapter,
-  BocomDebitAdapter,
-  BocDebitAdapter
+    AbcDebitAdapter,
+    CmbCreditAdapter,
+    CmbDebitAdapter,
+    BocomCreditAdapter,
+    BocomDebitAdapter,
+    BocDebitAdapter,
+    BocCreditAdapter
 ].sort(
-  (a, b) => compareKey(a.key, b.key)
+    (a, b) => compareKey(a.key, b.key)
 );
 
 export const AdapterKeys = AdapterList.map((a) => a.key);
 
 export const AdapterMap = AdapterList.reduce<Record<Adapter['key'], Adapter>>(
-  (pre, cur) => {
-    pre[cur.key] = cur;
-    return pre;
-  },
-  {}
+    (pre, cur) => {
+        pre[cur.key] = cur;
+        return pre;
+    },
+    {}
 );
